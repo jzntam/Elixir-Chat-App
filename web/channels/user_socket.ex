d9500318fa31f6a9b3ce851a0bb37bd5fd62df69
Jason Tam <jzntam@gmail.com>
2016-09-05 00:12:45 -0700

    Add room_channel

--- web/channels/user_socket.ex
+++ web/channels/user_socket.ex
@@ -2,11 +2,11 @@ defmodule ElixirChatServer.UserSocket do
   use Phoenix.Socket
 
   ## Channels
-  # channel "room:*", ElixirChatServer.RoomChannel
+  channel "room:*", ElixirChatServer.RoomChannel
 
   ## Transports
   transport :websocket, Phoenix.Transports.WebSocket
-  # transport :longpoll, Phoenix.Transports.LongPoll
+  transport :longpoll, Phoenix.Transports.LongPoll
 
   # Socket params are passed from the client and can
   # be used to verify and authenticate a user. After
